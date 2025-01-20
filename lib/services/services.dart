6d4335417a23f34df81809d2c256cbeeb97de23c
zakir1999 <m.zakirhossen99@gmail.com>
2025-01-20 10:02:38 +0600

    Getx file add

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,1 +1,10 @@
 import 'package:http/http.dart' as http;
+
+import '../Get/product_get_model.dart';
+
+class RemoteServices{
+  static var client=http.Client();
+  static Future<List<Product>?>fetchProducts() async{
+    var response=await client.get(Uri.parse("https://fakestoreapi.com/products"));
+  }
+}
